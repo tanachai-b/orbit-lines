434b4c7099e93db764ca920f10c634be8ba04bea
tanachaib <tanachai.bun@gmail.com>
2022-02-05 03:58:43 +0700

    prep fix loop logic

--- main.js
+++ main.js
@@ -2,37 +2,40 @@
 
 function main() {
 
-    let sun = new Celestial(
-        null,
-        new Orbit(0, 0, 0, 0, 0),
-        0
-    );
+    let sun = new Celestial(1000);
+    
     let mercury = new Celestial(
+        1,
         sun,
         new Orbit(100, 0, 0, 0, 0),
         0
     );
     let venus = new Celestial(
+        1,
         sun,
         new Orbit(200, 0, 0, 0, 0),
         0
     );
     let earth = new Celestial(
+        1,
         sun,
         new Orbit(400, 0, 0, 0, 0),
         0
     );
     let moon = new Celestial(
-        sun,
-        new Orbit(10, 0, 0, 0, 0),
+        1,
+        earth,
+        new Orbit(50, 0, 0, 0, 0),
         0
     );
     let mars = new Celestial(
+        1,
         sun,
         new Orbit(800, 0, 0, 0, 0),
         0
     );
     let jupiter = new Celestial(
+        1,
         sun,
         new Orbit(3200, 0, 0, 0, 0),
         0
@@ -112,29 +115,38 @@ class Camera {
 }
 
 class Celestial {
-    constructor(parent, orbit, trueAnomaly) {
+    constructor(mass, parent, orbit, trueAnomaly) {
+        this.mass = mass;
         this.parent = parent;
         this.orbit = orbit;
         this.trueAnomaly = trueAnomaly;
 
-        this.position = this.orbit.getPosition(this.trueAnomaly);
-        this.velocity = 0;
+        this.position = new Vector(0, 0, 0);
+        this.velocity = new Vector(0, 0, 0);
+
+        if (this.parent != null) {
+            this.position = this.orbit.getPosition(this.trueAnomaly).plus(this.parent.position);
+        }
     }
 
     update() {
         if (this.parent == null) return;
 
+        let relativePosition = this.position.minus(this.parent.position);
+
+
         let apoapsis = this.orbit.apoapsis;
         let orbitDirection = this.orbit.getPosition(1).minus(apoapsis);
 
-        let position2 = this.position.overVector(apoapsis.unit(), orbitDirection);
+
+        let position2 = relativePosition.overVector(apoapsis.unit(), orbitDirection);
         this.trueAnomaly = Math.atan2(position2.y, position2.x);
 
-        let orbitalSpeed = Math.sqrt(1000 * (2 / this.position.minus(this.parent.position).magnitude() - 1 / this.orbit.semiMajorAxis));
+        let orbitalSpeed = Math.sqrt(this.parent.mass * (2 / relativePosition.magnitude() - 1 / this.orbit.semiMajorAxis));
         this.velocity = this.orbit.getPosition(this.trueAnomaly + 0.000001).minus(this.orbit.getPosition(this.trueAnomaly)).unit().times(orbitalSpeed);
 
-        let angularVelocity = this.velocity.overVector(this.position.unit(), this.velocity).y / this.position.magnitude();
-        this.position = this.orbit.getPosition(this.trueAnomaly + angularVelocity);
+        let angularVelocity = this.velocity.overVector(relativePosition.unit(), this.velocity).y / relativePosition.magnitude();
+        this.position = this.orbit.getPosition(this.trueAnomaly + angularVelocity).plus(this.parent.position);
     }
 
     draw(camera) {
@@ -144,13 +156,13 @@ class Celestial {
         ctx.strokeStyle = '#FFFFFF';
         ctx.lineWidth = 1;
 
-        let complex = Complex.projectFrom3d(this.position, camera);
+        let posProj = Complex.projectFrom3d(this.position, camera);
 
         ctx.beginPath();
-        ctx.arc(complex.x, complex.y, 5, 0, 2 * Math.PI);
+        ctx.arc(posProj.x, posProj.y, 5, 0, 2 * Math.PI);
         ctx.stroke();
 
-        this.orbit.draw(camera);
+        if (this.orbit != null) { this.orbit.draw(camera, this.parent.position); }
     }
 }
 
@@ -201,7 +213,7 @@ class Orbit {
         return position4;
     }
 
-    draw(camera) {
+    draw(camera, position) {
         /** @type {HTMLCanvasElement} */
         let canvas = document.getElementById('canvas');
         let ctx = canvas.getContext('2d');
@@ -211,8 +223,8 @@ class Orbit {
 
         for (let i = 0; i < this.positions.length - 1; i++) {
 
-            let aProj = Complex.projectFrom3d(this.positions[i], camera);
-            let bProj = Complex.projectFrom3d(this.positions[i + 1], camera);
+            let aProj = Complex.projectFrom3d(this.positions[i].plus(position), camera);
+            let bProj = Complex.projectFrom3d(this.positions[i + 1].plus(position), camera);
 
             ctx.beginPath();
             ctx.moveTo(aProj.x, aProj.y);
@@ -220,14 +232,14 @@ class Orbit {
             ctx.stroke();
         }
 
-        let apoProj = Complex.projectFrom3d(this.apoapsis, camera);
+        let apoProj = Complex.projectFrom3d(this.apoapsis.plus(position), camera);
         ctx.strokeRect(apoProj.x - 3, apoProj.y - 3, 6, 6);
         ctx.fillRect(apoProj.x - 3, apoProj.y - 3, 6, 6);
 
-        let periProj = Complex.projectFrom3d(this.periapsis, camera);
+        let periProj = Complex.projectFrom3d(this.periapsis.plus(position), camera);
         ctx.strokeRect(periProj.x - 3, periProj.y - 3, 6, 6);
 
-        let ascProj = Complex.projectFrom3d(this.ascending, camera);
+        let ascProj = Complex.projectFrom3d(this.ascending.plus(position), camera);
         ctx.beginPath();
         ctx.moveTo(ascProj.x, ascProj.y - 5);
         ctx.lineTo(ascProj.x + 4, ascProj.y + 2);
@@ -235,7 +247,7 @@ class Orbit {
         ctx.closePath();
         ctx.stroke();
 
-        let descProj = Complex.projectFrom3d(this.descending, camera);
+        let descProj = Complex.projectFrom3d(this.descending.plus(position), camera);
         ctx.beginPath();
         ctx.moveTo(descProj.x, descProj.y + 5);
         ctx.lineTo(descProj.x + 4, descProj.y - 2);
@@ -250,8 +262,8 @@ class Orbit {
         }
 
         for (let i = 0; i < this.ascDescLine.length - 1; i++) {
-            let start = Complex.projectFrom3d(this.ascDescLine[i], camera);
-            let end = Complex.projectFrom3d(this.ascDescLine[i + 1], camera);
+            let start = Complex.projectFrom3d(this.ascDescLine[i].plus(position), camera);
+            let end = Complex.projectFrom3d(this.ascDescLine[i + 1].plus(position), camera);
 
             ctx.beginPath();
             ctx.moveTo(start.x, start.y);
